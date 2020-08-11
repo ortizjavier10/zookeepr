cc3374d49724afff48e696d137c9c4ba5e217ccf
JAVI <ortiz.javier10@gmail.com>
2020-08-10 20:09:58 -0400

    port number change

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const { animals } = require('./data/animals');
 
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -51,6 +55,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log('API server is now on port 3001!');
+app.listen(PORT, () => {
+    console.log(`API server is now on port ${PORT}!`);
 });
